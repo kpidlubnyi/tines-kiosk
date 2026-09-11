8ea81a67b13a4c2095f95672b55952e40f1d6fe7
kpidlubnyi <kpidlubnyi@gmail.com>
2026-09-11 09:59:11 +0200

    language temp

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -47,21 +47,7 @@
             <AppIcon name="arrow-left" class="back-to-offer-icon" />
           </button>
 
-          <button class="lang-btn" title="Переключити мову" @click="$emit('toggle-language')">
-            <svg 
-              class="globe-icon" 
-              viewBox="0 0 24 24" 
-              fill="none" 
-              stroke="currentColor" 
-              stroke-width="2" 
-              stroke-linecap="round" 
-              stroke-linejoin="round"
-            >
-              <circle cx="12" cy="12" r="10"></circle>
-              <line x1="2" y1="12" x2="22" y2="12"></line>
-              <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10z"></path>
-            </svg>
-          </button>
+          <LanguageSelector />
         </div>
       </div>
     </aside>
@@ -73,6 +59,7 @@ import SidebarNav from './SidebarNav.vue'
 import OfferSidebarNav from './OfferSidebarNav.vue'
 import CrossCategoryNav from './CrossCategoryNav.vue'
 import AppIcon from './AppIcon.vue'
+import LanguageSelector from './LanguageSelector.vue'
 
 export default {
   name: 'AppSidebar',
