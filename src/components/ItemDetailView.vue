2a9e22b523aa0bea76bf1444bddd45c7886607d6
kpidlubnyi <kpidlubnyi@gmail.com>
2026-09-14 13:54:02 +0200

    podstawowe funkcjonalności

--- src/components/ItemDetailView.vue
+++ src/components/ItemDetailView.vue
@@ -6,23 +6,22 @@
         class="media-column" 
         :class="{ 'full-width': isSidebarCollapsed }"
       >
-        <div class="media-wrapper">
-          <SketchfabViewer 
-            v-if="activeSketchfabId || item.sketchfabUrl"
-            :key="activeSketchfabId"
-            :modelId="activeSketchfabId"
-            :url="item.sketchfabUrl"
-            :title="langStore.getText(item.title)"
-            :autoplay="true"
-          />
-          <img 
-            v-else-if="item.image" 
-            :src="item.image" 
-            :alt="langStore.getText(item.title)" 
-            class="detail-image" 
-          />
-        </div>
-
+<!-- ЗМІНИ В СЕКЦІЇ <template> -->
+<div class="media-wrapper">
+  <SketchfabAPIViewer 
+    v-if="activeSketchfabId"
+    :key="activeSketchfabId"
+    :modelId="activeSketchfabId"
+    :autoplay="true"
+    :annotations="item.annotations"
+  />
+  <img 
+    v-else-if="item.image" 
+    :src="item.image" 
+    :alt="langStore.getText(item.title)" 
+    class="detail-image" 
+  />
+</div>
         <!-- Галерея 3D-моделей під в'ювером -->
         <div v-if="hasMultipleModels" class="models-gallery">
           <button 
@@ -172,10 +171,12 @@
 import SketchfabViewer from './SketchfabViewer.vue'
 import AppIcon from './AppIcon.vue'
 import { useLanguageStore } from '@/stores/language.js';
+import SketchfabAPIViewer from './SketchfabAPIViewer.vue';
 
 export default {
   name: 'ItemDetailView',
   components: {
+    SketchfabAPIViewer,
     SketchfabViewer,
     AppIcon
   },
